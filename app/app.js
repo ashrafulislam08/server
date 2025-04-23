ea64070a815d375c06ad28f2e2c3dac610ab08c0
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-04-23 12:44:16 +0600

    Folder structure for nodejs project

---
+++ app/app.js
@@ -0,0 +1,13 @@
+require("dotenv").config("../.env");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+
+app.use([morgan("dev"), cors()]);
+
+app.get("/health", (_req, res) => {
+  res.status(200).json({ message: "Success" });
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,98 +1,11 @@
-const express = require("express");
+require("dotenv").config();
+const http = require("http");
+const app = require("./app/app");
 
-const app = express();
+const server = http.createServer(app);
 
-const simpleLogger = (req, res, next) => {
-  console.log(` ${req.url} - ${req.method} - ${new Date().toISOString()}`);
-  next();
-};
+const PORT = process.env.PORT || 3000;
 
-app.use(simpleLogger)
-
-const books = [
-  {
-    id: 1,
-    name: "Be a Monster",
-    author: "Karim Khan",
-    price: 200,
-  },
-  {
-    id: 2,
-    name: "Think Intellectually",
-    author: "Sam Bolt",
-    price: 560,
-  },
-  {
-    id: 3,
-    name: "King of Kingdom",
-    author: "Becon Hosnar",
-    price: 1000,
-  },
-  {
-    id: 4,
-    name: "Be Like a Robot",
-    author: "Mubarak Kha",
-    price: 520,
-  },
-  {
-    id: 5,
-    name: "Art Of Thinking Clearly",
-    author: "Mubarak Kha",
-    price: 320,
-  },
-  {
-    id: 6,
-    name: "Making a Billion Dollar Startup",
-    author: "Monalisa",
-    price: 150,
-  },
-  {
-    id: 7,
-    name: "100+ Business Ideas",
-    author: "Ashraful Islam",
-    price: 720,
-  },
-  {
-    id: 8,
-    name: "How to Influence Friends and People",
-    author: "Jan Knawsar",
-    price: 320,
-  },
-  {
-    id: 9,
-    name: "Stupid Marketing ",
-    author: "John Hammar",
-    price: 320,
-  },
-  {
-    id: 10,
-    name: "Art Of Thinking Clearly",
-    author: "Mubarak Kha",
-    price: 320,
-  },
-];
-
-app.get("/books", (req, res) => {
-  if (req.query.show === "all") return res.json(books);
-  if (req.query.price == "500") {
-    const result = books.filter((book) => book.price <= 500);
-    res.json(result);
-  }
-  if (req.query.price == "1000") {
-    const result = books.filter((book) => book.price <= 1000);
-    res.json(result);
-  }
-  res.json(books.slice(0, 5));
-});
-
-app.get("/hello", (req, res) => {
-  res.json({ message: "Hello" });
-});
-
-app.get("/", (req, res) => {
-  res.json({ message: "Sweet Home" });
-});
-
-app.listen(3000, () => {
-  console.log(`Server is running on port: 3000`);
+server.listen(PORT, () => {
+  console.log(`Application is running on port ${PORT}`);
 });
