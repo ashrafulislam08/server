8522be2c13d4635ce202c08ca38ad2b3bd3badca
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-04-23 13:21:32 +0600

    Error handler added

--- app/app.js
+++ app/app.js
@@ -1,25 +1,15 @@
 require("dotenv").config("../.env");
 const express = require("express");
 
+const { notFoundHandler, errorHandler } = require("./error.js");
+
 const app = express();
 
 app.use(require("./middleware.js"));
 app.use(require("./routes.js"));
 
-app.use((_req, _res, next) => {
-  const error = new Error("Resource not found");
-  error.status = 404;
-  next(error);
-});
+app.use(notFoundHandler);
 
-app.use((error, _req, res, _next) => {
-  if (error.status) {
-    res.status(error.status).json({
-      message: error.message,
-    });
-  }
-
-  res.status(500).json({ message: "Something went wrong" });
-});
+app.use(errorHandler);
 
 module.exports = app;
