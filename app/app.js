d9ae426688fe216ec8484fd17ad518d6a5a30e1d
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-04-23 13:16:01 +0600

    middleware and routes separted for app     route

--- app/app.js
+++ app/app.js
@@ -1,13 +1,25 @@
 require("dotenv").config("../.env");
 const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+
 const app = express();
 
-app.use([morgan("dev"), cors()]);
+app.use(require("./middleware.js"));
+app.use(require("./routes.js"));
 
-app.get("/health", (_req, res) => {
-  res.status(200).json({ message: "Success" });
+app.use((_req, _res, next) => {
+  const error = new Error("Resource not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, _req, res, _next) => {
+  if (error.status) {
+    res.status(error.status).json({
+      message: error.message,
+    });
+  }
+
+  res.status(500).json({ message: "Something went wrong" });
 });
 
 module.exports = app;
